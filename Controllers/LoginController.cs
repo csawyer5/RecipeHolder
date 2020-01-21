d4365f7137f6441bd2e0749dd39e32b2b4b8320a
Chris Sawyer <chris5sawyer@hotmail.com>
2020-01-21 15:10:35 -0600

    Initial Commit

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Recipe_Holder.Models;
+
+namespace Recipe_Holder.Controllers
+{
+    //blah blah
+}
